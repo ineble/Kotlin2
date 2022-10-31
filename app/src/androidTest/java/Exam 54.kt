5f5ee0ef7ce72629e53f26f6556c8ce59b34c856
ineble <thdwltjq94@gmail.com>
2022-10-31 10:39:30 +0900

    2022-10-31

---
+++ app/src/androidTest/java/Exam 54.kt
@@ -0,0 +1,11 @@
+fun lablBreak(){
+    println("labelBreak")
+    for(i in 1..5){
+        second@ for (j in 1..5){
+            if(j ==3) break
+            println("i: $i, j:$j")
+        }
+        println("after for j")
+    }
+    println("after for i")
+}
